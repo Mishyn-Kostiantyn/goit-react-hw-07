d8da873b47b5257bb608669f65698beaa08ed836
Mishyn-Kostiantyn <129659279+Mishyn-Kostiantyn@users.noreply.github.com>
2024-04-19 18:42:00 +0300

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,15 @@
+import { useSelector} from 'react-redux';
+import css from './ContactList.module.css';
+import Contact from '../Contact/Contact';
+
+function ContactList() {
+  const contactsList = useSelector(state => state.selectContacts.items);
+  const filter = useSelector(state => state.selectNameFilter.name);
+ 
+  const filteredContactList = contactsList.filter((contact) =>contact.name.toLowerCase().includes(filter.toLowerCase()));
+  return (
+    <div className={css.contactListContainer}>{filteredContactList.map((contactData) => (<Contact key={contactData.id} contactData={contactData} /> ))}</div>
+  )
+}
+
+export default ContactList
---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,20 @@
+
+import css from './SearchBox.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import {changeFilter} from '../../redux/filtersslice.js'
+function SearchBox( ) {
+    const filter = useSelector(state => state.selectNameFilter.name);
+  const dispatch = useDispatch();
+
+const handleChange=(evt)=>{ dispatch(changeFilter(evt.target.value)) }
+  return (
+    <div className={css.searchBoxContainer}>
+      <label htmlFor="searchBar" className={css.searchBoxLabel}>Find contacts by name</label>
+      <input type="text" className={css.searchBoxInput} id="searchBar" placeholder='Search....' value={filter} onChange={handleChange}/>
+     
+    </div>
+  );
+}
+
+
+export default SearchBox
